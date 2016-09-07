a4dd378d1b80f3fdd9dcfa04a8b4d154ffcf44bb
Armani Raleigh <armanijolee@gmail.com>
2016-09-07 17:00:38 -0400

    Change Number 2

--- src/GameOne.java
+++ src/GameOne.java
@@ -3,8 +3,17 @@ import java.util.Scanner;
 public class GameOne {
 
 	public static void main(String[] args) {
+		
+		
+		
 		// TODO Auto-generated method stub
-		System.out.println( "Welcome!! What is your name? ");
+		System.out.println( " Welcome!! What is your name? ");
+		System.out.print( " Enter name here -> ");
+		Scanner input = new Scanner (System.in);
+		
+		
+		
+		
 	}
 
 }
