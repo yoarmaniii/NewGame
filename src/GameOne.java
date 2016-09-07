3e09f685b3fc96517bca212d1639dd14edf82611
Armani Raleigh <armanijolee@gmail.com>
2016-09-07 16:50:22 -0400

    Initializing Game First Change

---
+++ src/GameOne.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class GameOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( "Welcome!! What is your name? ");
+	}
+
+}
