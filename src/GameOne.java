805f56c1826e6d65d243e6679fae7623cb90bf7c
Armani Raleigh <armanijolee@gmail.com>
2016-09-07 17:22:01 -0400

    Change 4, user inputs Yes

--- src/GameOne.java
+++ src/GameOne.java
@@ -1,3 +1,4 @@
+import java.text.ChoiceFormat;
 import java.util.Scanner;
 
 public class GameOne {
@@ -5,15 +6,21 @@ public class GameOne {
 	public static void main(String[] args) {
 		
 		
-		
+		Scanner input = new Scanner (System.in);
 		// TODO Auto-generated method stub
 		System.out.println( " Welcome!! What is your name? ");
 		System.out.print( " Enter name here -> ");
 		String name;
-		Scanner input = new Scanner (System.in);
+		String choice = "Yes";
 		name = input.nextLine();
 		System.out.println();
 		
+		System.out.println( "Would you like to play a game?");
+		System.out.println("Type Yes or No");
+
+		
+		while (choice.equals("Yes")); {
+		}
 		
 		
 		
