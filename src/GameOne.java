eb2af91fb63e430db1d932ae4c2326ccdb7ea3f4
Armani Raleigh <armanijolee@gmail.com>
2016-09-07 17:11:06 -0400

    Change 3 Name

--- src/GameOne.java
+++ src/GameOne.java
@@ -9,7 +9,10 @@ public class GameOne {
 		// TODO Auto-generated method stub
 		System.out.println( " Welcome!! What is your name? ");
 		System.out.print( " Enter name here -> ");
+		String name;
 		Scanner input = new Scanner (System.in);
+		name = input.nextLine();
+		System.out.println();
 		
 		
 		
