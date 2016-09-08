9843b64a63cfd7d92f9e20aa229f5b38333b8322
Armani Raleigh <armanijolee@gmail.com>
2016-09-08 17:05:52 -0400

    Do you face the beast question answered

--- src/GameOne.java
+++ src/GameOne.java
@@ -8,23 +8,39 @@ public class GameOne {
 		String userName;
 		String choice;
 		userName = user.nextLine();
+		boolean userContinue = true;
 		System.out.println();
 		
-		System.out.println(userName + "," +" "+ "would you like to see your future?");	
-		choice = user.nextLine();
-		boolean userContinue = true;
-	
-		
-			if(choice.equals("yes"))
-			{	System.out.println("Cool! Lets begin your Sci-Fi journey to the future!");
-				
-			} 
-			else 
-			{ 	System.out.println( "We will see you when you are ready! Goodbye" +" " + userName + "!");
-				
-			}
+		System.out.println(userName + "," +" "+ "would you like to see your future? Yes/No?");	
+		//Get user input for next line
+		choice = user.nextLine();	
+		//See what choice the user makes to start game
+			if(choice.equalsIgnoreCase("No"))
+			{	System.out.println( "We will see you when you are ready! Goodbye" +" " + userName + "!");
+
+			} else {
+				if(choice.equalsIgnoreCase("Yes"))
+			 	System.out.println("Cool! Lets begin your Sci-Fi journey to the future!");
+				}
+			System.out.println();
+			System.out.println("You are walking across a lovely desert field and you encounter one of Godzilla's children! He's hungry!! Uh OH!!");
+			System.out.println("What do you do?");
+			choice = user.nextLine();
 			
-		
-		
-	}
+			if(choice.equalsIgnoreCase("Face Him"))
+			{	System.out.println("You approach Godzilla's child! You see that he has __ heads");
+				System.out.println("How many heads does he have?");
+				choice = user.nextLine();
+				
+			
+			}
+			else {
+				if(choice.equalsIgnoreCase("Run Away"))
+					System.out.println("You have left the fantasy game! Thanks for playing" + userName);
+			}
+			}	
 }
+		
+		
+		
+	
