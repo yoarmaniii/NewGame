449deac80e4d769f3d32124b7321ac197db549df
Armani Raleigh <armanijolee@gmail.com>
2016-09-08 22:45:24 -0400

    Almost done, it runs all the way through

--- src/GameOne.java
+++ src/GameOne.java
@@ -55,20 +55,78 @@ public class GameOne {
 						  System.out.println("You named him" + choice);
 						  System.out.println(userName + "and" + choice);
 						  System.out.println("They became great friends and went in search of their parents together!! THE END");
+						  break;
+					  }
+					}
+				}
+			}
+							String head2;
+							head2 = choice;
+							switch(head2){
+							case "2":
+								
+								System.out.println("No one has ever seen this creature before with 2 heads! What a discovery! He seems friendly!");
+								System.out.println("What do you want to feed him? fish, veggies, a building");
+								choice = user.nextLine();
+								if(choice.equalsIgnoreCase("veggies"));
+								{ System.out.println("You give him the veggies and He HATES it!");
+								  System.out.println("He approaches you and he wants you to stare into his __eyes");
+								  System.out.println("what color eyes does he have? Green, Black, Yellow?");
+								  choice = user.nextLine();
+								  String Black;
+								  Black = choice;
+								  if(choice.equalsIgnoreCase("Black")){
+
+								  System.out.println("He has black eyes, and they are soo beautiful!");
+									  System.out.println("Last part of your future is that you give him a name. What's his name?");
+									  choice = user.nextLine();
+									  System.out.println("You named him" + " " + choice);
+									  System.out.println(userName + " "  + "and" + " " + choice);
+									  System.out.println("They became great friends and went in search of their parents together!! THE END");
+									  break;
 					  }
 					  }
 				}
+							String head3;
+							head3 = choice;
+							switch(head3){
+							case "3":
+								
+								System.out.println("No one has ever seen this creature before with 3 heads! Are you sure its one of Godzilla's children?? What a discovery! He seems friendly though!");
+								System.out.println("What do you want to feed him? fish, veggies, a building");
+								choice = user.nextLine();
+								if(choice.equalsIgnoreCase("a building"));
+								{ System.out.println("I dont think he can do that JUUUSSSTTT yet! Good try though!");
+								  System.out.println("He approaches you and he wants you to stare into his __eyes");
+								  System.out.println("what color eyes does he have? Green, Black, Yellow?");
+								  choice = user.nextLine();
+								  String Yellow;
+								  Yellow = choice;
+								  if(choice.equalsIgnoreCase("Yellow")){
+
+								  System.out.println("He has yellow eyes, and they are soo beautiful!");
+									  System.out.println("Last part of your future is that you give him a name. What's his name?");
+									  choice = user.nextLine();
+									  System.out.println("You named him" + " " + choice);
+									  System.out.println(userName + " "  + "and" + " " + choice);
+									  System.out.println("They became great friends and went in search of their parents together!! THE END");
+									  break;
 			}
-			else
-			{
-				if(choice.equalsIgnoreCase("Run Away"))
-					System.out.println("You have left the fantasy game! Thanks for playing" + userName);
+								}
+			}
+										
+					
+					
+									if(choice.equalsIgnoreCase("Run Away"))
+									System.out.println("You have left the fantasy game! Thanks for playing" + userName);
+					 }
 			}
 			}	
-				}
-}
+
+
 	
+			
 	
-		
-		
+
+	
 	
